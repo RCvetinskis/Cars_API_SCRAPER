d96f1829b174c7d0966a7f28ab2b7101961de07e
Robertas <100841685+RCvetinskis@users.noreply.github.com>
2024-02-17 22:34:23 +0200

    motos scrapped

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const app = express();
 require("dotenv").config();
 
 const carsData = require("../cars.json");
+const motosData = require("../motos.json");
 
 app.get("/api/cars", (req, res) => {
   const carsWithoutModels = carsData.map(({ brand }) => ({ brand }));
@@ -16,6 +17,19 @@ app.get("/api/cars/:brand", (req, res) => {
     (car) => car.brand.toLowerCase() === brand
   );
   res.json(filteredCars);
+});
+app.get("/api/motos", (req, res) => {
+  const motosWithoutModels = motosData.map(({ brand }) => ({ brand }));
+
+  res.json(motosWithoutModels);
+});
+
+app.get("/api/motos/:brand", (req, res) => {
+  const brand = req.params.brand.toLowerCase();
+  const filteredMotos = motosData.filter(
+    (moto) => moto.brand.toLowerCase() === brand
+  );
+  res.json(filteredMotos);
 });
 
 module.exports = app;
